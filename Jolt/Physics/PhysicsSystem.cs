960d8b7a7071fc084b4f15d877d939d00e7e9a6f
Chris Parker <seep@sent.com>
2024-04-21 12:37:12 -0400

    Add PhysicsSystem#GetBodyInterfaceNoLock and doc comments

--- Jolt/Physics/PhysicsSystem.cs
+++ Jolt/Physics/PhysicsSystem.cs
@@ -12,10 +12,19 @@ namespace Jolt
 
         internal NativeHandle<JPH_BodyActivationListener> BodyActivationListenerHandle;
 
+        /// <summary>
+        /// The ObjectLayerPairFilter of the system.
+        /// </summary>
         public ObjectLayerPairFilter ObjectLayerPairFilter;
 
+        /// <summary>
+        /// The BroadPhaseLayerInterface of the system.
+        /// </summary>
         public BroadPhaseLayerInterface BroadPhaseLayerInterface;
 
+        /// <summary>
+        /// The ObjectVsBroadPhaseLayerFilter of the system.
+        /// </summary>
         public ObjectVsBroadPhaseLayerFilter ObjectVsBroadPhaseLayerFilter;
 
         public PhysicsSystem(PhysicsSystemSettings settings)
@@ -38,9 +47,20 @@ namespace Jolt
             JPH_PhysicsSystem_OptimizeBroadPhase(Handle);
         }
 
+        /// <summary>
+        /// Get the body interface, which allows creating and removing bodies and changing their properties.
+        /// </summary>
         public BodyInterface GetBodyInterface()
         {
             return new BodyInterface(JPH_PhysicsSystem_GetBodyInterface(Handle));
+        }
+
+        /// <summary>
+        /// Get a non-locking version of the body interface. Use with great care!
+        /// </summary>
+        public BodyInterface GetBodyInterfaceNoLock()
+        {
+            return new BodyInterface(JPH_PhysicsSystem_GetBodyInterfaceNoLock(Handle));
         }
 
         public void SetContactListener(IContactListener listener)
@@ -60,22 +80,34 @@ namespace Jolt
         /// <summary>
         /// Update the physics system. Returns true if there were no errors.
         /// </summary>
+        /// <remarks>
+        /// The out parameter will contain the error if any.
+        /// </remarks>
         public bool Step(float deltaTime, int collisionSteps, out PhysicsUpdateError error)
         {
             error = JPH_PhysicsSystem_Step(Handle, deltaTime, collisionSteps);
             return error == PhysicsUpdateError.None;
         }
 
+        /// <summary>
+        /// Get the current number of bodies in the body manager.
+        /// </summary>
         public uint GetNumBodies()
         {
             return JPH_PhysicsSystem_GetNumBodies(Handle);
         }
 
+        /// <summary>
+        /// Get the current number of active bodies in the body manager.
+        /// </summary>
         public uint GetNumActiveBodies(BodyType type)
         {
             return JPH_PhysicsSystem_GetNumActiveBodies(Handle, type);
         }
 
+        /// <summary>
+        /// Get the maximum number of bodies that this system supports.
+        /// </summary>
         public uint GetMaxBodies()
         {
             return JPH_PhysicsSystem_GetMaxBodies(Handle);
@@ -104,6 +136,18 @@ namespace Jolt
             JPH_BodyActivationListener_Destroy(BodyActivationListenerHandle);
         }
 
+        #region IEquatable
+
+        public static bool operator ==(PhysicsSystem lhs, PhysicsSystem rhs)
+        {
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator !=(PhysicsSystem lhs, PhysicsSystem rhs)
+        {
+            return !lhs.Equals(rhs);
+        }
+
         public bool Equals(PhysicsSystem other)
         {
             return Handle == other.Handle;
@@ -118,5 +162,7 @@ namespace Jolt
         {
             return Handle.GetHashCode();
         }
+
+        #endregion
     }
 }
