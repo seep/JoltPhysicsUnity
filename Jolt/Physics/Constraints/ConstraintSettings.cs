b99bf61541cc428da1aa16b5c3e6533bf04e801c
Chris Parker <seep@sent.com>
2024-06-08 11:39:23 -0400

    Generate JPH_ConstraintSettings bindings for ConstraintSettings

--- Jolt/Physics/Constraints/ConstraintSettings.cs
+++ Jolt/Physics/Constraints/ConstraintSettings.cs
@@ -1,6 +1,6 @@
 ﻿namespace Jolt
 {
-    [GenerateHandle]
+    [GenerateHandle, GenerateBindings("JPH_ConstraintSettings")]
     public readonly partial struct ConstraintSettings
     {
         internal readonly NativeHandle<JPH_ConstraintSettings> Handle;
