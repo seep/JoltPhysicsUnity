053a887d589c5d9ac07f78c94f9776199f3e81cc
Chris Parker <seep@sent.com>
2024-07-27 15:04:24 -0400

    Add implicit conversions into Constraint handle

--- Jolt/Physics/Constraints/Constraint.cs
+++ Jolt/Physics/Constraints/Constraint.cs
@@ -9,5 +9,15 @@
         {
             Handle = handle;
         }
+
+        public static implicit operator Constraint(DistanceConstraint constraint)
+        {
+            return new Constraint(constraint.Handle.Reinterpret<JPH_Constraint>());
+        }
+
+        public static implicit operator Constraint(FixedConstraint constraint)
+        {
+            return new Constraint(constraint.Handle.Reinterpret<JPH_Constraint>());
+        }
     }
 }
