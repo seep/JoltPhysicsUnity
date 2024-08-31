c3389a9355b3094a8a7f836a8a68ce04b06a7ccf
Chris Parker <seep@sent.com>
2024-08-31 00:42:04 -0700

    Add JPH_PlaneShape and JPH_PlaneShapeSettings bindings

---
+++ Jolt/Bindings/Bindings_JPH_PlaneShape.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Mathematics;
+
+namespace Jolt
+{
+    internal static unsafe partial class Bindings
+    {
+        public static NativeHandle<JPH_PlaneShape> JPH_PlaneShape_Create(Plane plane, float halfExtent)
+        {
+            // TODO include JPH_PhysicsMaterial argument
+            
+            return CreateHandle(UnsafeBindings.JPH_PlaneShape_Create(&plane, default, halfExtent));
+        }
+
+        public static Plane JPH_PlaneShape_GetPlane(NativeHandle<JPH_PlaneShape> shape)
+        {
+            Plane result = default;
+
+            UnsafeBindings.JPH_PlaneShape_GetPlane(shape, &result);
+
+            return result;
+        }
+        
+        public static float JPH_PlaneShape_GetHalfExtent(NativeHandle<JPH_PlaneShape> shape)
+        {
+            return UnsafeBindings.JPH_PlaneShape_GetHalfExtent(shape);
+        }
+    }
+}
---
+++ Jolt/Bindings/Bindings_JPH_PlaneShapeSettings.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Mathematics;
+
+namespace Jolt
+{
+    internal static unsafe partial class Bindings
+    {
+        public static NativeHandle<JPH_PlaneShapeSettings> JPH_PlaneShapeSettings_Create(Plane plane, float halfExtent)
+        {
+            // TODO include JPH_PhysicsMaterial argument
+            
+            return CreateHandle(UnsafeBindings.JPH_PlaneShapeSettings_Create(&plane, default, halfExtent));
+        }
+
+        public static NativeHandle<JPH_PlaneShape> JPH_PlaneShapeSettings_CreateShape(NativeHandle<JPH_PlaneShapeSettings> settings)
+        {
+            return CreateHandle(UnsafeBindings.JPH_PlaneShapeSettings_CreateShape(settings));
+        }
+    }
+}
