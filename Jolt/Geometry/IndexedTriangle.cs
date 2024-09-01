d3908b4570271371f423b119ea18592e00a48b3c
Chris Parker <seep@sent.com>
2024-09-01 19:19:33 -0400

    Fix missing user data in IndexedTriangle struct layout

--- Jolt/Geometry/IndexedTriangle.cs
+++ Jolt/Geometry/IndexedTriangle.cs
@@ -12,20 +12,24 @@ namespace Jolt
 
         public readonly uint MaterialIndex;
 
-        public IndexedTriangle(uint i1, uint i2, uint i3, uint materialIndex)
+        public readonly uint UserData;
+
+        public IndexedTriangle(uint i1, uint i2, uint i3, uint materialIndex, uint userData)
         {
             I1 = i1;
             I2 = i2;
             I3 = i3;
 
             MaterialIndex = materialIndex;
+
+            UserData = userData;
         }
 
         #region IEquatable
 
         public bool Equals(IndexedTriangle other)
         {
-            return I1 == other.I1 && I2 == other.I2 && I3 == other.I3 && MaterialIndex == other.MaterialIndex;
+            return I1 == other.I1 && I2 == other.I2 && I3 == other.I3 && MaterialIndex == other.MaterialIndex && UserData == other.UserData;
         }
 
         public override bool Equals(object obj)
@@ -35,7 +39,7 @@ namespace Jolt
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(I1, I2, I3, MaterialIndex);
+            return HashCode.Combine(I1, I2, I3, MaterialIndex, UserData);
         }
 
         public static bool operator ==(IndexedTriangle lhs, IndexedTriangle rhs)
