fcc10008d364a07497b0de0e6bbb3c362fde52c2
Chris Parker <seep@sent.com>
2024-09-02 18:22:39 -0400

    Add JPH_PhysicsMaterial safe bindings

---
+++ Jolt/Bindings/Bindings_JPH_PhysicsMaterial.cs
@@ -0,0 +1,17 @@
+﻿namespace Jolt
+{
+    internal static unsafe partial class Bindings
+    {
+        public static NativeHandle<JPH_PhysicsMaterial> JPH_PhysicsMaterial_Create()
+        {
+            return CreateHandle(UnsafeBindings.JPH_PhysicsMaterial_Create());
+        }
+        
+        public static void JPH_PhysicsMaterial_Destroy(NativeHandle<JPH_PhysicsMaterial> material)
+        {
+            UnsafeBindings.JPH_PhysicsMaterial_Destroy(material);
+            
+            material.Dispose();
+        }
+    }
+}
