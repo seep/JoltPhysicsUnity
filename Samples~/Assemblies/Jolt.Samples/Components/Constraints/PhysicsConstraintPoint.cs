7907d7bb6fae802ff8f5fe67c16a65f3dadb4d30
Chris Parker <seep@sent.com>
2024-09-04 22:43:51 -0400

    Add point constraint sample scene

---
+++ Samples~/Assemblies/Jolt.Samples/Components/Constraints/PhysicsConstraintPoint.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Mathematics;
+
+namespace Jolt.Samples
+{
+    public class PhysicsConstraintPoint : PhysicsConstraintBase
+    {
+        public PhysicsBody BodyA;
+        public PhysicsBody BodyB;
+
+        public float3 Point;
+
+        internal override Constraint Initialize(ManagedPhysicsContext context)
+        {
+            var settings = PointConstraintSettings.Create();
+
+            settings.SetPoint1(Point);
+            settings.SetPoint2(Point);
+            
+            var ba = context.ManagedToNative[BodyA];
+            var bb = context.ManagedToNative[BodyB];
+
+            return settings.CreateConstraint(ba, bb);
+        }
+    }
+}
