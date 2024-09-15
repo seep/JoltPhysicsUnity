baf21e1cfbb36c7c935292797aae3417ccfffbf3
Chris Parker <seep@sent.com>
2024-09-15 11:56:17 -0400

    Add contact manifold bindings

---
+++ Jolt/Bindings/Bindings_JPH_ContactManifold.cs
@@ -0,0 +1,48 @@
+﻿using Unity.Mathematics;
+
+namespace Jolt
+{
+    internal static unsafe partial class Bindings
+    {
+        public static float3 JPH_ContactManifold_GetWorldSpaceNormal(NativeHandle<JPH_ContactManifold> manifold)
+        {
+            float3 result;
+            UnsafeBindings.JPH_ContactManifold_GetWorldSpaceNormal(manifold, &result);
+            return result;
+        }
+
+        public static float JPH_ContactManifold_GetPenetrationDepth(NativeHandle<JPH_ContactManifold> manifold)
+        {
+            return UnsafeBindings.JPH_ContactManifold_GetPenetrationDepth(manifold);
+        }
+        
+        public static uint JPH_ContactManifold_GetSubShapeID1(NativeHandle<JPH_ContactManifold> manifold)
+        {
+            return UnsafeBindings.JPH_ContactManifold_GetSubShapeID1(manifold);
+        }
+
+        public static uint JPH_ContactManifold_GetSubShapeID2(NativeHandle<JPH_ContactManifold> manifold) 
+        {
+            return UnsafeBindings.JPH_ContactManifold_GetSubShapeID2(manifold);
+        }
+
+        public static uint JPH_ContactManifold_GetPointCount(NativeHandle<JPH_ContactManifold> manifold) 
+        {
+            return UnsafeBindings.JPH_ContactManifold_GetPointCount(manifold);
+        }
+
+        public static rvec3 JPH_ContactManifold_GetWorldSpaceContactPointOn1(NativeHandle<JPH_ContactManifold> manifold, uint index)
+        {
+            rvec3 result;
+            UnsafeBindings.JPH_ContactManifold_GetWorldSpaceContactPointOn1(manifold, index, &result);
+            return result;
+        }
+
+        public static rvec3 JPH_ContactManifold_GetWorldSpaceContactPointOn2(NativeHandle<JPH_ContactManifold> manifold, uint index) 
+        {
+            rvec3 result;
+            UnsafeBindings.JPH_ContactManifold_GetWorldSpaceContactPointOn1(manifold, index, &result);
+            return result;
+        }
+    }
+}
