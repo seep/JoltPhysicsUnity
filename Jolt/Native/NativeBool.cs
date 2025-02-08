6008fbae4e8ae8022798440d7602bab35c794ab7
Chris Parker <seep@sent.com>
2025-02-08 16:19:14 -0500

    Represent NativeBool as a byte

--- Jolt/Native/NativeBool.cs
+++ Jolt/Native/NativeBool.cs
@@ -1,11 +1,13 @@
-﻿namespace Jolt
+﻿using System;
+
+namespace Jolt
 {
     /// <summary>
     /// A uint with implicit bool casting, providing consistent interop with native "bool" uints.
     /// </summary>
-    internal struct NativeBool
+    public struct NativeBool : IEquatable<NativeBool>
     {
-        public uint Value;
+        public byte Value;
 
         public static implicit operator bool(NativeBool value)
         {
@@ -14,7 +16,25 @@
 
         public static implicit operator NativeBool(bool value)
         {
-            return new NativeBool { Value = value ? 1u : 0u };
+            return new NativeBool { Value = value ? (byte)1 : (byte)0 };
         }
+
+        public static bool operator false(NativeBool value) => value.Value == 0;
+        
+        public static bool operator true(NativeBool value) => value.Value != 0;
+
+        public static bool operator ==(NativeBool lhs, NativeBool rhs) => lhs.Value == rhs.Value;
+
+        public static bool operator !=(NativeBool lhs, NativeBool rhs) => lhs.Value != rhs.Value;
+        
+        #region IEquatable
+
+        public bool Equals(NativeBool other) => Value == other.Value;
+
+        public override bool Equals(object obj) => obj is NativeBool other && Equals(other);
+
+        public override int GetHashCode() => Value.GetHashCode();
+
+        #endregion
     }
 }
