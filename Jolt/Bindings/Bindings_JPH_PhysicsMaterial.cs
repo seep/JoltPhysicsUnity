b4f67dce3bacdd47bfec31da0f05d6ddf20f56c1
Chris Parker <seep@sent.com>
2025-02-08 18:18:01 -0500

    Fix up Bindings_JPH_PhysicsMaterial

--- Jolt/Bindings/Bindings_JPH_PhysicsMaterial.cs
+++ Jolt/Bindings/Bindings_JPH_PhysicsMaterial.cs
@@ -1,16 +1,28 @@
-﻿namespace Jolt
+﻿using System.Runtime.InteropServices;
+
+namespace Jolt
 {
     internal static unsafe partial class Bindings
     {
-        public static NativeHandle<JPH_PhysicsMaterial> JPH_PhysicsMaterial_Create()
+        public static NativeHandle<JPH_PhysicsMaterial> JPH_PhysicsMaterial_Create(string name, uint color)
         {
-            return CreateHandle(UnsafeBindings.JPH_PhysicsMaterial_Create());
+            nint nameptr;
+            
+            try
+            {
+                nameptr = Marshal.StringToHGlobalAnsi(name); // TODO look for a way to generate unsafe binding with marshalling attributes
+                
+                return CreateHandle(UnsafeBindings.JPH_PhysicsMaterial_Create((sbyte*)nameptr, color));
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(nameptr);
+            }
         }
         
         public static void JPH_PhysicsMaterial_Destroy(NativeHandle<JPH_PhysicsMaterial> material)
         {
             UnsafeBindings.JPH_PhysicsMaterial_Destroy(material);
-            
             material.Dispose();
         }
     }
