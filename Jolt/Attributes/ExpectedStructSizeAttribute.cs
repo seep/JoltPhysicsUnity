2fe9d71d1c3c7cafd3c7e1b03ff92912db2af74a
Chris Parker <seep@sent.com>
2025-02-08 19:01:30 -0500

    Add expected struct size attribute

---
+++ Jolt/Attributes/ExpectedStructSizeAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Jolt
+{
+    /// <summary>
+    /// Indicates a struct should have the same size and layout as another struct.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Struct)]
+    internal class ExpectedStructSizeAttribute : Attribute
+    {
+        public Type Type;
+        
+        public ExpectedStructSizeAttribute(Type type)
+        {
+            Type = type;
+        }
+    }
+}
